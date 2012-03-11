0f6fe5316d9767cec579a62e017cd8919ef24074
mewsplayground <mewsplayground@09f15e90-c184-4a26-aad6-528d2e8cadf7>
2012-03-11 14:15:55 +0000

    git-svn-id: http://mewsplayground-svn.cvsdude.com/seaweeds/trunk/bsponmpi@293 09f15e90-c184-4a26-aad6-528d2e8cadf7

--- opts_naphta.cov.warwick.ac.uk_Darwin_i386.py
+++ opts_naphta.cov.warwick.ac.uk_Darwin_i386.py
@@ -1,3 +1,4 @@
 tbbdir = '/Users/peterkrusche/workspace/tbb40_20120201oss'
-additional_cflags='-I/opt/local/include'
-additional_lflags='-L/opt/local/lib -lcblas'
+#additional_cflags='-I/opt/local/include'
+additional_cflags='-I/opt/atlas3.9.69/include -I/opt/local/include'
+additional_lflags='-L/opt/atlas3.9.69/lib /opt/atlas3.9.69/lib/libatlas.a /opt/atlas3.9.69/lib/libcblas.a'
