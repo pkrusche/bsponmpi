b395695638e2d6ec236a2d537cdbfcf3fe58f981
mewsplayground <mewsplayground@09f15e90-c184-4a26-aad6-528d2e8cadf7>
2012-03-14 10:26:49 +0000

    git-svn-id: http://mewsplayground-svn.cvsdude.com/seaweeds/trunk/bsponmpi@321 09f15e90-c184-4a26-aad6-528d2e8cadf7

---
+++ examples/benchmark/benchmarkdata.h
@@ -0,0 +1,142 @@
+/*
+BSPonMPI. This is an implementation of the BSPlib standard on top of MPI
+Copyright (C) 2006  Wijnand J. Suijlen, 2012 Peter Krusche
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+See the AUTHORS file distributed with this library for author contact
+information.
+*/
+
+
+/** @file benchmarkdata.h
+
+Helper class to determine computation or communication rates and latencies 
+from a set of samples.
+
+@author Peter Krusche
+*/
+
+#ifndef __benchmarkdata_H__
+#define __benchmarkdata_H__
+
+#include <iostream>
+#include <list>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics.hpp> 
+
+namespace benchmark {
+	
+	/** This class handles benchmark data statistics */
+	class BenchmarkData {
+	public:
+		struct Sample {
+			int n;
+			double rate;
+		};
+		
+		/** 
+		 * Each sample has: a problem size (n), and a rate. 
+		 */
+
+		inline void add_sample (int n, double r) {
+			Sample s;
+			s.n = n;
+			s.rate = r;
+			samples.push_back(s);
+		}
+
+		/** output a table of rates and errors. */
+		inline void ratetable ( std::ostream & output ) {
+			std::map <int, psize_info > sample_map;
+			collect( sample_map );
+
+			std::vector<int> ns;
+			for (std::map<int, psize_info>::iterator it = sample_map.begin(), it_end = sample_map.end();
+				it != it_end; ++it) {
+				ns.push_back( it->first );
+			}
+			std::sort (ns.begin(), ns.end());
+			output << "n\tmean\tmin\tmax\terr" << std::endl;
+			for (std::vector<int> :: iterator it = ns.begin(), it_end = ns.end(); it != it_end; ++it) {
+				psize_info & p = sample_map[*it];
+				output
+					<< p.n << "\t" 
+					<< p.tavg << "\t" 
+					<< p.tmin << "\t" 
+					<< p.tmax << "\t" 
+					<< p.err << std::endl;
+			}
+		}
+
+	private:
+
+		struct psize_info {
+			int n;
+			double tmin;
+			double tmax;
+			double tavg;
+			int samples;
+			double err;
+			std::list<double> _samples;
+		};
+		
+		/**
+		 * Collect samples and statistical information into a map.
+		 */
+
+		void collect ( std::map <int, psize_info > & sample_map ) {
+			for (std::vector<Sample>::iterator it = samples.begin(), it_end = samples.end();
+				it != it_end; ++it) {
+				std::map<int, psize_info>::iterator it2 = sample_map.find (it->n);
+				if (it2 == sample_map.end()) {
+					psize_info p;
+					sample_map[it->n] = p;
+				}
+				psize_info & pr = sample_map[it->n];
+				pr.n = it->n;
+				pr._samples.push_back(it->rate);
+			}
+
+			using namespace boost;
+			using namespace boost::accumulators;
+
+			for (std::map<int, psize_info>::iterator it = sample_map.begin(), it_end = sample_map.end();
+				it != it_end; ++it) {
+				accumulator_set<double, stats<	tag::min, tag::max, tag::mean, tag::error_of<tag::mean> > > acc1;
+
+				for (std::list<double>::iterator it2 = it->second._samples.begin(), it2_end = it->second._samples.end(); 
+					 it2 != it2_end; ++it2) {
+					acc1( *it2 );
+				}
+
+				it->second.tmin = min(acc1);
+				it->second.tmax = max(acc1);
+				it->second.tavg = mean(acc1);
+				it->second.err = error_of<tag::mean> (acc1); 
+				it->second.samples = (int)it->second._samples.size();
+			}
+		}
+
+		std::vector<Sample> samples;
+	};
+}
+
+#endif
---
+++ examples/benchmark/benchmarkfactory.h
@@ -0,0 +1,122 @@
+/*
+BSPonMPI. This is an implementation of the BSPlib standard on top of MPI
+Copyright (C) 2006  Wijnand J. Suijlen, 2012 Peter Krusche
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+See the AUTHORS file distributed with this library for author contact
+information.
+*/
+
+
+/** @file benchmarkfactory.h
+
+Maintain a list of benchmarks and their description.
+
+@author Peter Krusche
+*/
+
+#ifndef __Benchmarkfactory_h__
+#define __Benchmarkfactory_h__ 
+
+#include <boost/bind.hpp>
+
+namespace benchmark {
+	/** Abstract benchmark base class */
+	class AbstractBenchmark {
+	public:
+		/** Run on problem size n, return a rate of operations (complexity(n) / time(n)) */
+		virtual double run (int n) = 0;		
+	};
+	
+	/** Benchmark factory */
+	class BenchmarkFactory {
+	public:
+		typedef boost::function0< AbstractBenchmark* > b_factory;
+		struct description {
+			std::string desc;
+			b_factory factory;
+		};
+
+	    static BenchmarkFactory & get_instance() {
+			static BenchmarkFactory f;
+			return f;
+		}
+			
+		inline void reg (const char * name, const char * desc, b_factory f) {
+			description mbd;
+			mbd.desc = std::string (desc);
+			mbd.factory = f;
+			map[std::string(name)] = mbd;
+		}
+
+		inline void remove (const char * name) {
+			map.erase (map.find(name));
+		}
+	
+		inline AbstractBenchmark* create (const char * name) {
+			if (map.find(name) == map.end()) {
+				throw std::runtime_error ("Unknown benchmark type.");
+			}
+			return map[name].factory();
+		}
+
+		inline std::string describe (const char * name) {
+			if (map.find(name) == map.end()) {
+				throw std::runtime_error ("Unknown benchmark type.");
+			}
+			return map[name].desc;
+		}
+	
+		inline void list(std::ostream & out) {
+			for (std::map<std::string, description>::iterator it = map.begin(), it_end = map.end();
+					it != it_end; ++it) {
+				out << "\t" << it->first << "\t" << it->second.desc << std::endl;
+			}
+		}
+	
+	private:
+	    BenchmarkFactory() {}
+
+	    std::map<std::string, description> map;
+	};	
+
+/** Static registration helper */
+template <class B>
+struct BenchmarkReg {
+	static benchmark::AbstractBenchmark * create () {
+		static B b;
+		return &b;
+	}
+	
+	BenchmarkReg(const char * name, const char * desc) {
+		my_name = name;
+		benchmark::BenchmarkFactory::get_instance().reg(name, desc, 
+			boost::bind(&BenchmarkReg<B>::create));
+	}
+	
+	~BenchmarkReg() {
+		benchmark::BenchmarkFactory::get_instance().remove(my_name.c_str());
+	}
+	
+	std::string my_name;
+};
+
+#define REGISTER_BENCHMARK(name, desc, benchmarkclass) \
+	static benchmark::BenchmarkReg< benchmark::benchmarkclass > _br##benchmarkclass (name, desc); 
+
+};
+
+#endif
