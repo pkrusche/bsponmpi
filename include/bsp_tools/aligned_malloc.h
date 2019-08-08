1f4f6d78c64a0179b88a85dcf6d53b516020dc57
Peter Krusche <pkrusche@gmail.com>
2019-08-08 09:38:22 +0200

    link fix for inline functions

--- include/bsp_tools/aligned_malloc.h
+++ include/bsp_tools/aligned_malloc.h
@@ -1,21 +1,21 @@
 /*
     BSPonMPI. This is an implementation of the BSPlib standard on top of MPI
     Copyright (C) 2006  Wijnand J. Suijlen
-                                                                                
+
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
     version 2.1 of the License, or (at your option) any later version.
-                                                                                
+
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Lesser General Public License for more details.
-                                                                                
+
     You should have received a copy of the GNU Lesser General Public
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-                                                                                
+
     See the AUTHORS file distributed with this library for author contact
     information.
 */
@@ -25,7 +25,7 @@
 
 #include <stdlib.h>
 
-inline void *aligned_malloc(size_t size, int alignment) {
+static inline void *aligned_malloc(size_t size, int alignment) {
 	char *block = malloc(size + 2*alignment + 4);
 
 	char** aligned = (char**)(((size_t)block + alignment + sizeof(char*)) & (~(alignment-1)));
@@ -35,11 +35,11 @@ inline void *aligned_malloc(size_t size, int alignment) {
 }
 
 
-inline void aligned_free(void *memory) {
+static inline void aligned_free(void *memory) {
 	free(((char**)memory)[-1]);
 }
 
-inline int is_aligned_memory(void * v, int alignment) {
+static inline int is_aligned_memory(void * v, int alignment) {
 	return (((size_t)v) & (alignment-1)) == 0;
 }
 
